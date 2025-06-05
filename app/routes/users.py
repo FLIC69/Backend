c3cc915a4b7f4718cf07de74ddc93791c7b43c62
Luis Garza <lgarzan@amazon.com>
2025-06-04 19:55:07 -0600

    https support

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from app.utils import getDb, auth
 from app.db import DB
 from datetime import timedelta
@@ -15,7 +15,7 @@ def root(db: DB = Depends(getDb.get_db)):
 
 
 @router.post("/login")
-def login(data: UserLogin, db: DB = Depends(getDb.get_db)):
+def login(data: UserLogin, response: Response, db: DB = Depends(getDb.get_db)):
     try:
         result = db.execute_query(
             "SELECT id, username, password FROM users WHERE username = %s", 
@@ -47,6 +47,17 @@ def login(data: UserLogin, db: DB = Depends(getDb.get_db)):
         )
 
         auth.log_db(db, user[0], "/users/login", "POST")  
+
+        # Set cookie in the response
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,  # Change to True if you're using HTTPS
+            samesite="lax",  # Or "strict", or "none" if using cross-site
+            max_age=access_token_expires.total_seconds(),
+            path="/"
+        )
 
         return {
             "access_token": access_token,
